98a39f20d7edf32f84c824c2ce44ced0c18c82db
Mohamed Azekri <mohamed.azekri@student.rocva.nl>
2021-12-28 13:16:04 +0100

    opdracht 2.05

--- 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
+++ 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
@@ -1,19 +1,29 @@
 -- Opdracht 1 
+SELECT Max(wage) As hoogste_loon_speler_fc_utrecht FROM players WHERE club = "FC utrecht";
 
 -- Opdracht 2 
+SELECT ROUND(AVG(wage)) AS avg_wage FROM players;
 
 -- Opdracht 3
+SELECT SUM(wage) AS sum_wage_fcgroningen FROM players WHERE club="fc groningen";
 
 -- Opdracht 4
+SELECT COUNT(*) AS aantal_spelers_manchester FROM players WHERE club="Manchester City" AND club= "Manchester United";
 
 -- Opdracht 5
+SELECT AVG(wage) AS avg_wage_netherlands FROM players WHERE nationality = "Netherlands";
 
 -- Opdracht 6 
+SELECT AVG(wage) AS avg_wage_under_20 FROM players WHERE age < 20;
 
 -- Opdracht 7 
+SELECT AVG(wage) AS avg_wage_above_20 FROM players WHERE age > 20;
 
 -- Opdracht 8
+SELECT SUM(value) AS sum_value_chelsea FROM players WHERE club="Chelsea";
 
 -- Opdracht 9
+SELECT ROUND(avg(age)) AS round_avg_age FROM players;
 
 -- Opdracht 10
+SELECT club , sum(wage) AS sum_wage_liverpool , round(avg(value)) as avg_value_liverpool FROM players WHERE club="Liverpool";
